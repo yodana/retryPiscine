a3e8a8e96728f19b3aa5124ce5778ec40fd4cb6c
yodana <dana.yohann@gmail.com>
2018-11-06 23:30:41 +0000

    Ajout du dernier ex Bug a voir et a corriger

--- ex27/srcs/main.c
+++ ex27/srcs/main.c
@@ -1,15 +1,25 @@
 #include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
 	char *buf;
+	int fd;
 
 	buf = 0;
 	if (argc == 1)
 		write(1, "File name missing.", 18);
 	else if (argc == 3)
 		write(1, "Too many arguments", 18);
-	else 
+	else
 	{
-		while(read(argv[1],buf,1))
+		fd = open(argv[1], O_RDONLY);
+		while (read(fd, &buf, 1))
+		{
+			write(1, &buf, 1);
+		}
+		close(fd);
+	}
+	return (0);
+}
 
